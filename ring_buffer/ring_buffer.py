ce02e9ea86de8e5613764fc4719482ad1985feb0
Landon Turner <lturner@teamworks.com>
2020-07-17 11:13:26 -0400

    pass capacity tests

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,12 +1,14 @@
 class RingBuffer:
     def __init__(self, capacity):
-        self.capacity = capacity
+        self.capacity = capacity # 5
         self.storage = []
 
     def append(self, item):
         # see if storage is at capacity (use modulo to check)
         # if it is, overwrite the eldest value
         # else append the item
+        self.storage.append(item)
 
     def get(self):
         # return storage
+        return self.storage
