1f710c1e644565498e04e9a9dc2704bd0755013b
Landon Turner <lturner@teamworks.com>
2020-07-17 14:01:42 -0400

    pass buffer

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -3,23 +3,18 @@ from queue import Queue
 class RingBuffer:
     def __init__(self, capacity):
         self.capacity = capacity # 5
-        self.storage = [] * capacity
+        self.storage = [None] * capacity
         self.index = 0
 
 
     def append(self, item):
         # first attempt was close
-        # see if storage is empty
-        self.index + 1
-        if not self.storage:
-            self.storage.append(item)
-        buffer = len(self.storage) % self.capacity 
-        if buffer == 0:
-            self.storage[0] = item
-            self.storage.pop(0)
-        else:
-            self.storage.append(item)
+        self.storage[self.index] = item
+        self.index += 1
+        if self.index == self.capacity:
+            self.index = 0
 
     def get(self):
         # return storage
-        return self.storage
+        item = [item for item in self.storage if item is not None]
+        return item
