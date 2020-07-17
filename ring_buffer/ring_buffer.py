ea25e605295a5ecad0610d5aeab5fe3d0783ce12
Landon Turner <lturner@teamworks.com>
2020-07-17 13:21:49 -0400

    reverting attempts

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,13 +1,19 @@
+from queue import Queue
+
 class RingBuffer:
     def __init__(self, capacity):
         self.capacity = capacity # 5
         self.storage = []
+        self.head = 0
 
     def append(self, item):
         # first attempt was close
         # see if storage is empty
+        # if not self.storage:
+        #     self.storage.enqueue(item.value)
         if not self.storage:
             self.storage.append(item)
+
         elif len(self.storage) > 0:
             buffer = len(self.storage) % self.capacity
             if buffer == 0:
